ff5a1b545ae2765e905dd2503f511b04ff2c618f
phuhoang.06 <hoangvanphu2006@gmail.com>
2024-11-04 20:13:59 +0700

    add

---
+++ src/LeapYear.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year;
+        System.out.print("Enter year: ");
+        year = scanner.nextInt();
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0 && year % 100 == 0) {
+            System.out.println("Leap Year");
+        }
+        else {
+            System.out.println("Not a leap year");
+        }
+    }
+}
